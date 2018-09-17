519807066927cb9761182b53f457606f417f8214
Enrique Vega <enriquevega@Enrique-Vega.localdomain>
2018-09-17 15:33:11 -0700

    adds public page

---
+++ src/pages/PublicExample.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PublicExample extends Component {
+  render() {
+    return (
+      <div> This page is Public!</div>
+    )
+  }
+}
+
+export default PublicExample
